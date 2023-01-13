54a1d2f99a59b8f638fa637ac53aa6a4fa84748b
HunterBresler <hunterwayneb@icloud.com>
2023-01-13 00:27:17 -0500

    Created

---
+++ Prog2/BreslerHunter_Assignment0.cpp
@@ -0,0 +1,17 @@
+/*
+    Author: Hunter Bresler
+    Date: 1/12/2022
+    Assignment0: "Hello World"
+*/
+
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+
+    cout << "Setting up vscode was a pain :'(";
+
+    return 0;
+}
