66d8ea5c2c96a17524ab805bd81c8735c439dd86
HunterBresler <hunterwayneb@icloud.com>
2023-02-27 18:08:22 -0500

    asd

--- CIS/Assignment2_DES.cpp
+++ CIS/Assignment2_DES.cpp
@@ -11,37 +11,93 @@
 using namespace std;
 
 
-//*Function Definitions
-void GetInput(string* plainText, string* key)
+class KEY 
 {
-    //Get Plain Text
-    cout << "\nEnter the message you want encoded:\n\t";
-    cin >> *plainText;
+    private:
 
-    //Get Master Key
-    cout << "\nEnter the master key:\n\t";
-    cin >> *key;
-}
+        //*Declare member variables
+        int var1;
+        int var2;
+        int var3;
+        int var4;
+        int var5;
 
+    public:
+        //*Constructor
+        KEY();
 
+        //*Getter Functions
+        int get_var1();
+        int get_var2();
+        int get_var3();
+        int get_var4();
+        int get_var5();
 
-//*Turn the plain text into binary
-void PlainTextToBinary(string PlainText, string* ptBINARY)
-{
+        //*Setter Functions
+        void set_var1(int var1);
+        void set_var2(int var2);
+        void set_var3(int var3);
+        void set_var4(int var4);
+        void set_var5(int var5);
+};
 
-}
 
 
 
 //*MAIN
 int main(){
-
-    //Get Plain Text and Key
-    string PlainText, ptBINARY, KeyMASTER;
-    GetInput(&PlainText, &KeyMASTER);
-    //Format Plain Text to Binary
-    PlainTextToBinary(PlainText, &ptBINARY);
-
-
     return 0;
+}
+
+
+/*
+    !This is the list of functions in the KEY Class 
+    !The functions will be folded below
+*/ 
+//*Constructor
+KEY::KEY()
+{
+
+}
+
+//*Getter Functions
+int KEY::get_var1() {
+    return var1;
+}
+
+int KEY::get_var2() {
+    return var2;
+}
+
+int KEY::get_var3() {
+    return var3;
+}
+
+int KEY::get_var4() {
+    return var4;
+}
+
+int KEY::get_var5() {
+    return var5;
+}
+
+//*Setter Functions
+void KEY::set_var1(int new_var1) {
+    var1 = new_var1;
+}
+
+void KEY::set_var2(int new_var2) {
+    var2 = new_var2;
+}
+
+void KEY::set_var3(int new_var3) {
+    var3 = new_var3;
+}
+
+void KEY::set_var4(int new_var4) {
+    var4 = new_var4;
+}
+
+void KEY::set_var5(int new_var5) {
+    var5 = new_var5;
 }
